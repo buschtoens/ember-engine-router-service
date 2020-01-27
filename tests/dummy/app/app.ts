ee356ab7e97b7a0d2eb33c3202651ff6386ad714
Jan Buschtöns <jan@buschtoens.me>
2020-01-27 15:41:00 +0100

    chore(build): setup `ember-cli-typescript`

--- tests/dummy/app/app.ts
+++ tests/dummy/app/app.ts
@@ -1,12 +1,14 @@
 import Application from '@ember/application';
-import Resolver from 'ember-resolver';
-import loadInitializers from 'ember-load-initializers';
-import config from './config/environment';
 
-export default class App extends Application {
+import loadInitializers from 'ember-load-initializers';
+
+import config from './config/environment';
+import Resolver from './resolver';
+
+export default class DummyApp extends Application {
   modulePrefix = config.modulePrefix;
   podModulePrefix = config.podModulePrefix;
   Resolver = Resolver;
 }
 
-loadInitializers(App, config.modulePrefix);
+loadInitializers(DummyApp, config.modulePrefix);
--- tests/dummy/app/router.ts
+++ tests/dummy/app/router.ts
@@ -1,10 +1,10 @@
 import EmberRouter from '@ember/routing/router';
+
 import config from './config/environment';
 
-export default class Router extends EmberRouter {
+export default class DummyRouter extends EmberRouter {
   location = config.locationType;
   rootURL = config.rootURL;
 }
 
-Router.map(function() {
-});
+DummyRouter.map(function() {});
