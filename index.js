c00283ab8c55764cc4412fe4f8cf8770e0af25e8
Jan Buschtöns <jan@buschtoens.me>
2020-02-24 15:51:42 +0100

    chore(build): assert being dependcy of engine

--- index.js
+++ index.js
@@ -1,5 +1,23 @@
 'use strict';
 
 module.exports = {
-  name: require('./package').name
+  name: require('./package').name,
+
+  included(parent) {
+    this._super(parent);
+
+    this.assertDependencyOfEngine(parent);
+  },
+
+  assertDependencyOfEngine(parent) {
+    if (
+      !typeof parent.treeForEngine === 'function' &&
+      parent.pkg.keywords &&
+      ['ember-addon', 'ember-engine'].any(k => parent.pkg.keywords.includes(k))
+    ) {
+      this.ui.writeErrorLine(
+        `${this.name}: Cannot be used in '${parent.name}', as it is not an engine.`
+      );
+    }
+  }
 };
